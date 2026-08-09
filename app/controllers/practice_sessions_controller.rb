c8922713fa8a2ceae424eaeb556c8d949a7d68d4
Jamdurk <james.savona@outlook.com>
2026-08-09 15:32:35 +1000

    Updated create action, now adds to DB and has proper flow

--- app/controllers/practice_sessions_controller.rb
+++ app/controllers/practice_sessions_controller.rb
@@ -7,9 +7,16 @@ class PracticeSessionsController < ApplicationController
         render json: PracticeSession.find(params[:id])
     end
 
-    def create 
-        render json: {message: "Hello, create!"}
-    end
+    def create
+        practice_session = PracticeSession.new(practice_session_params)
+      
+        if practice_session.save
+          render json: practice_session, status: :created
+        else
+          render json: { errors: practice_session.errors.full_messages },
+                 status: :unprocessable_entity
+        end
+      end
 
 
     private 
