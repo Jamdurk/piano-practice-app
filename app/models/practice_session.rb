a64468c528cc3d1b3dff9ae1dd6305f87b485d67
Jamdurk <james.savona@outlook.com>
2026-08-09 15:32:53 +1000

    Validations for prac sesh model

--- app/models/practice_session.rb
+++ app/models/practice_session.rb
@@ -1,2 +1,5 @@
 class PracticeSession < ApplicationRecord
+    validates :name, presence: true
+    validates :started_at, presence: true
+    validates :duration_seconds, presence: true, numericality: { greater_than: 0 }
 end
