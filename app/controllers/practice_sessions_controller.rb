055b6b1b706a032647edcfe14e78d0c9ae6900a0
Jamdurk <james.savona@outlook.com>
2026-08-09 13:58:36 +1000

    Updated show action to query DB, and created params from it

--- app/controllers/practice_sessions_controller.rb
+++ app/controllers/practice_sessions_controller.rb
@@ -4,11 +4,18 @@ class PracticeSessionsController < ApplicationController
     end
 
     def show 
-        render json: { message: "Hello, show!" }
+        render json: PracticeSession.find(params[:id])
     end
 
     def create 
         render json: {message: "Hello, create!"}
+    end
+
+
+    private 
+
+    def practice_session_params
+        params.require(:practice_session).permit(:name, :started_at, :duration_seconds)
     end
 
 end
