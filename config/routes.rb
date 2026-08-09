b68ec6beabaa554376e5192b85653f6b669cbab3
Jamdurk <james.savona@outlook.com>
2026-08-09 16:10:19 +1000

    Stats controller, dashboard api and route for stats

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/practice_sessions", to: "practice_sessions#index"
   get "/practice_sessions/:id", to: "practice_sessions#show"
   post "/practice_sessions", to: "practice_sessions#create"
-
+  
+  get "/stats", to: "stats#show"
 end
